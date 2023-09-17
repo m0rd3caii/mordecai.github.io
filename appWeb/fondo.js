dbfc585edaf79ddf7eaa2581ec4a51af3e953ad2
m0rd3cai <142639542+m0rd3caii@users.noreply.github.com>
2023-09-18 00:49:19 +0200

    Delete appWeb directory

--- appWeb/fondo.js
+++
@@ -1,8 +0,0 @@
-const elementsToResetCursor = document.querySelectorAll('.reset-cursor');
-
-// Agrega un evento de escucha para restablecer el cursor al predeterminado
-elementsToResetCursor.forEach((element) => {
-    element.addEventListener('mouseenter', () => {
-        document.body.style.cursor = 'auto';
-    });
-});
